a79c436f6de3198e11e6b1bc403c4b4c96372db5
veer-chheda <veerchheda3525@gmail.com>
2024-02-08 22:15:02 +0530

    added loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import gsap from 'gsap'
 
 function App() {
+
+  useEffect(() => {
+    let t1 = gsap.timeline();
+    t1.from(".veer", {
+      opacity: 1,
+      x: 10,
+      delay: 0.5,
+      duration: 1,
+      ease: "power1.out",
+      yoyo: true,
+    });
+  
+    t1.from(
+      ".dot",
+      {
+        opacity: 0,
+        delay: 0.5,
+        repeatDelay: 1,
+        duration: 1,
+        ease: "power1.out",
+      },
+      0.01
+    );
+  
+    t1.to(".dot", {
+      opacity:1,
+      x: 20,
+      duration: 1,
+      ease: "power1.out",
+    });
+  
+    t1.to(".dot", {
+      x: 0,
+      duration: 0.5,
+      ease: "power1.out",
+    });
+
+    t1.to(".veer", {
+      opacity: 0,
+      xPercent: 0,
+      duration: 1,
+      ease: "power1.out",
+      yoyo: true,
+    });
+  
+    t1.to(
+      ".dot",
+      {
+        opacity: 0,
+        duration: 1,
+        ease: "expo.out",
+      },
+      3
+    );
+  
+    t1.to(
+      ".cover",
+      {
+        xPercent: -100,
+        duration: 1,
+        ease: "power1.out",
+      },
+      3
+    );
+
+    t1.to(
+      ".cover-2",
+      {
+        xPercent: -100,
+        duration: 1,
+        ease: "power1.out",
+      },
+      3.2
+    );
+    
+    t1.to(
+      ".cover-3",
+      {
+        xPercent: -100,
+        duration: 1,
+        ease: "power1.out",
+      },
+      3.4
+    );
+    
+    t1.to(
+      ".cover-4",
+      {
+        xPercent: -100,
+        duration: 1,
+        ease: "power1.out",
+      },
+      3.6
+    );
+    
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="cover-4"></div>
+          <div className="cover-3"></div>
+          <div className="cover-2"></div>
+    <div className="cover">
+      <div className="cover-heading">
+        <h1 className="veer">veer</h1>
+        <h1 className="dot">.</h1>
+      </div>
+    </div>
+
+    <div className="veer2">veer chheda</div>
     </div>
   );
 }
